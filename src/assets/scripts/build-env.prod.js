6764c77a0f295d6b8d4afd30658b6a29911ecd79
Abel Gasque <abelgasque20@gmail.com>
2025-10-16 21:54:11 -0300

    Add env db

--- src/assets/scripts/build-env.prod.js
+++ src/assets/scripts/build-env.prod.js
@@ -10,6 +10,7 @@ const {
   GITHUB_USERNAME,
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DB_URL,
   FIREBASE_PROJECT_ID,
   FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGING_SENDER_ID,
@@ -36,7 +37,7 @@ try {
     firebase: {
       apiKey: FIREBASE_API_KEY,
       authDomain: FIREBASE_AUTH_DOMAIN,
-      databaseURL: `https://${FIREBASE_PROJECT_ID}.firebaseio.com`,
+      databaseURL: FIREBASE_DB_URL,
       projectId: FIREBASE_PROJECT_ID,
       storageBucket: FIREBASE_STORAGE_BUCKET,
       messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
